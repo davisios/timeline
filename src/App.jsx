de4fd3e13469faa434fab731b93405695211aaff
David Leon ALvarez <david.leon@nursa.com>
2024-11-13 20:38:55 -0600

    update start and end date instructions

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div className='App'>
         <h1>Event Timeline</h1>
         <h3>Double Click to Edit</h3>
-        <h3>Drag and Drop to Change Start Date</h3>
+        <h3>Drag edges to change Start and End Date</h3>
         <EventProvider>
           <Timeline />
         </EventProvider>
